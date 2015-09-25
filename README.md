5d18be17859a05d73cc5d357bcd03b7f47a8d2e5
Celien <cesliens@gmail.com>
2015-09-25 11:48:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple WebDriver class used to automate a Test on Korben.info. <br/>
+Automatically trust SSL certificates. <br />
+Selenium + PHPUnit + facebook/webDriver;
---
+++ tests/KorbenTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverKeys;
+
+class KorbenTest extends PHPUnit_Framework_TestCase{
+
+	protected $webDriver;
+	protected $url = 'https://korben.info'; 
+
+	public function setUp(){
+		$host = 'http://localhost:4444/wd/hub'; // this is the default
+		$capabilities = DesiredCapabilities::firefox();
+		$this->webDriver = RemoteWebDriver::create($host, DesiredCapabilities::firefox()); 
+	}
+
+	/*public function tearDown(){
+		$this->webDriver->close(); 
+	}*/
+
+	public function testGoogleSearch(){
+		// Get the url defined in vars
+		$this->webDriver->get($this->url);
+
+		// Find the search tab and select it 
+		$search = $this->webDriver->findElement(WebDriverBy::id('s')); 
+		$search->click(); 
+
+		// Enter some value in it
+		$this->webDriver->getKeyboard()->sendKeys('black hat'); 
+		$this->webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER); 
+
+		// Accet the SSL certificate
+		$this->webDriver->switchTo()->alert()->accept(); 
+	}
+
+}
